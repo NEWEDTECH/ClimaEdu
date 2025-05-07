0f385ea7fcab2a5989452c51809677e3c82ed5e4
lLuan Portugal <luanportugal.profissional.tec@gmail.com>
2025-05-07 04:09:24 -0300

    feat: adicionando componente de botão

---
+++ src/components/button/button.tsx
@@ -0,0 +1,40 @@
+import react, { ReactNode } from 'react'
+import { cn } from "@/lib/utils"
+import { Slot } from "@radix-ui/react-slot";
+
+type ButtonProps = {
+  children: ReactNode;
+  onClick?: () => void;
+  className?: string;
+  icon?: ReactNode;
+  iconPosition?: "start" | "end";
+  asChild?: boolean;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+export function Button({
+  children,
+  onClick,
+  className,
+  icon,
+  iconPosition = "start",
+  asChild = false,
+  ...props
+}: ButtonProps) {
+  const Comp = asChild ? Slot : "button";
+
+  return (
+    <Comp
+      onClick={onClick}
+      className={cn(
+        "cursor-pointer",
+        "inline-flex items-center justify-center gap-2 rounded-md border bg-white px-4 py-2 text-sm font-medium shadow-sm hover:bg-gray-100 transition-colors",
+        className
+      )}
+      {...props}
+    >
+      {icon && iconPosition === "start" && <span className="mr-2">{icon}</span>}
+      {children}
+      {icon && iconPosition === "end" && <span className="ml-2">{icon}</span>}
+    </Comp>
+  );
+}
