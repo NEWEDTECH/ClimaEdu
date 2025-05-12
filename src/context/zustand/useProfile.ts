77d87451f4fb531f9806ebb80cc4a3a69f45d12d
lLuan Portugal <luanportugal.profissional.tec@gmail.com>
2025-05-12 16:25:06 -0300

    feat: adicionando id do usuário

--- src/context/zustand/useProfile.ts
+++ src/context/zustand/useProfile.ts
@@ -3,18 +3,22 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
-type UserRole = 'student' | 'tutor' | 'admin' |null;
+type UserRole = 'student' | 'tutor' | 'admin' | null;
 
-interface ProfileState {
+type ProfileState = {
+  id: string;
   role: UserRole;
   setRole: (role: UserRole) => void;
+  setId: (id: string) => void;
 }
 
 export const useProfile = create<ProfileState>()(
   persist(
     (set) => ({
+      id: '',
       role: null,
       setRole: (role) => set({ role }),
+      setId: (id) => set({ id }),
     }),
     {
       name: 'profile-storage',
